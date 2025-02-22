df56c5005ea94210ba71fab7af44bee84c79e7b3
SooinDev <alwayswithsound@gmail.com>
2025-02-22 11:59:15 +0900

    fix: backend 폴더 Git 루트 재설정

---
+++ src/test/java/com/example/autofinder/AutofinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autofinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
