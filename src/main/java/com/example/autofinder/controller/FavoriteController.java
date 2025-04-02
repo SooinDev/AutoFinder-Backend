1074a7f9b9625cbe0012c8eeba27b4bca4f4ae7a
SooinDev <alwayswithsound@gmail.com>
2025-04-02 18:00:46 +0900

    fix: url

--- src/main/java/com/example/autofinder/controller/FavoriteController.java
+++ src/main/java/com/example/autofinder/controller/FavoriteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/favorites")
+@RequestMapping("/api/favorites")
 @RequiredArgsConstructor
 public class FavoriteController {
     private final FavoriteService favoriteService;
